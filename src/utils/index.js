940c2b0a74111807182c82fd25b703b4af2bc457
JC <j.c.ashley4363@gmail.com>
2023-12-12 19:45:02 -0600

    navbar buttons working. importing styles from utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -173,6 +173,11 @@ export const adminAddProductformControls = [
     },
 ];
 
+export const styles = {
+    button:
+        "mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white rounded",
+}
+
 export const AvailableSizes = [
     {
         id: "s",
