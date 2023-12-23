dbc604dca796ce3ef75bfbc433117b25a9261c7b
JC <j.c.ashley4363@gmail.com>
2023-12-22 22:01:59 -0600

    login service attempting but not working donno why

---
+++ src/services/login/index.js
@@ -0,0 +1,22 @@
+
+
+
+export const login = async (formData) => {
+    try {
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        });
+
+        console.log(response)
+
+        const data = await response.json();
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
