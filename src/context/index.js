ba570e5e1da66b5b3e2a612af5e8dd9b8885e136
JC <j.c.ashley4363@gmail.com>
2023-12-23 22:16:15 -0600

    added user to cookies tand local storage to keep user loggedin

--- src/context/index.js
+++ src/context/index.js
@@ -1,6 +1,7 @@
 'use client'
 
-import { createContext, useState } from "react";
+import Cookies from "js-cookie";
+import { createContext, useEffect, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
@@ -9,6 +10,21 @@ export default function GlobalState({ children }) {
     const [showNavModal, setShowNavModal] = useState(false);
     const [isAuthUser, setIsAuthUser] = useState(false);
     const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        console.log(Cookies.get('token'))
+
+        const userLoggedIn = Cookies.get('token')
+
+        if (userLoggedIn !== undefined) {
+            setIsAuthUser(isAuthUser)
+            const userData = JSON.parse(localStorage.getItem('user')) || {}
+            setUser(userData)
+        } else {
+            setIsAuthUser(false)
+        }
+
+    }, [Cookies])
 
     return (
         <GlobalContext.Provider value={{
