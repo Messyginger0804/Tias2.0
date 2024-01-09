bee58d14bf5c5f30e11d744fe3c0a2614d7399dc
JC <j.c.ashley4363@gmail.com>
2024-01-09 12:51:26 -0600

    file(created files productListings): created file structure for product listing and dummy data

---
+++ src/components/productListings/index.js
@@ -0,0 +1,65 @@
+'use client'
+
+
+// _id
+// 659d938902a508d9f3e109c8
+// name
+// "mens tshirt blue"
+// description
+// "slim fit blue mens t-shirt"
+// price
+// 19
+// category
+// "men"
+
+// sizes
+// Array (5)
+// deliveryInfo
+// "same day"
+// onSale
+// "yes"
+// priceDrop
+// 24
+// imageUrl
+// "https://firebasestorage.googleapis.com/v0/b/tias-thenewone.appspot.com…"
+// createdAt
+// 2024-01-09T18:42:17.731+00:00
+// updatedAt
+// 2024-01-09T18:42:17.731+00:00
+// __v
+// 0
+
+const fakeData = [
+    {
+
+        _id: 659d938902a508d9f3e109c8,
+        name: "mens tshirt blue",
+        description: "slim fit blue mens t-shirt",
+        price: 19,
+        category: "men",
+        sizes: [
+            {
+
+                id: "s",
+                label: "S"
+            }
+        ],
+        deliveryInfo: "same day",
+        onSale: "yes",
+        priceDrop: 24,
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/tias-thenewone.appspot.com…",
+    },
+];
+
+export default function ProductListing() {
+
+    return <section
+        className="bg-whtie py-12 sm:py-16"
+    >
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+            <div className="mt-10 grid grid-cols gap-6 sm:grid-cols-4 sm:gap-4 lg:mt-16">
+
+            </div>
+        </div>
+    </section>
+}
