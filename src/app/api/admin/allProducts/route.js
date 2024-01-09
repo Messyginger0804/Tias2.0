fb7bed6bbee9adbe6a4ccc5251af74c1f4a200ed
JC <j.c.ashley4363@gmail.com>
2024-01-09 15:38:20 -0600

    func(api/admin/allProducts): added allProduct-toroute with try/catch and errorr, success or fail messages

---
+++ src/app/api/admin/allProducts/route.js
@@ -0,0 +1,51 @@
+import Product from "@/models/products";
+import connectToDB from "@/mongodb";
+import { NextResponse } from "next/server";
+
+
+
+export const dynamic = 'force-dynamic';
+
+
+export async function GET(req) {
+
+    try {
+        await connectToDB();
+
+        const user = 'admin';
+
+        if (user === 'admin') {
+
+            const extractAllProducts = await Product.find({});
+
+            if (extractAllProducts) {
+                return NextResponse.json({
+                    success: true,
+                    data: extractAllProducts,
+                    message: "Welcome back admin. Here is your products to edit or delete.",
+                })
+            } else {
+                return NextResponse.json({
+                    success: false,
+                    status: 204,
+                    message: "No products found. Refresh your page if error presist contact support.",
+                });
+
+            }
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "You are not autorized for this action!",
+            });
+        }
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            success: false,
+            message: "Something went wrong ! Please try again later",
+        });
+    }
+}
+
+
