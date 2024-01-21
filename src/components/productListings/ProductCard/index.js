0db2a601b31ed0542a3d33187f31d9362c6820a5
JC <j.c.ashley4363@gmail.com>
2024-01-21 11:32:59 -0600

    perf(app/components/productListings/productCard/index.js): configure onsale pricing to show the reduced price how much it is onsale.

--- src/components/productListings/ProductCard/index.js
+++ src/components/productListings/ProductCard/index.js
@@ -7,37 +7,38 @@ function ProductCard(
     { item }
 ) {
     return (
-        <div
-            // onClick={() => router.push(`/product/${item._id}`)}
-            className="text-black">
-            <div className="overflow-hideen aspect-w-1 aspect-h-1 h-52 ">
+        <div onClick={() => router.push(`/product/${item._id}`)} className="text-black">
+            <div className="overflow-hideen aspect-w-1 aspect-h-1 h-52">
                 <img
                     src={item.imageUrl}
-                    alt={item.name}
-                    className="h-full w-full object-cover transition-all duration-300 hover:scale-110"
+                    alt="Product image"
+                    className="h-full w-full object-cover transition-all duration-300 group-hover:scale-125"
                 />
             </div>
-            <h3>{item.onsale}</h3>
             {item.onSale === "yes" ? (
-                <div className="absolute top-0 m-2 rounded-full bg-black">
-                    <p className="rounded-full  p-1 text-[8px] font-bold uppercase tracking-wide text-white sm:py-1 sm:px-3">
-                        Sale
-                    </p>
-                </div>
+                // <div className="absolute top-0 m-2 rounded-full bg-black">
+                <p className="rounded-full p-1 text-[8px] font-bold uppercase tracking-wide text-black sm:py-1 sm:px-3">
+                    Sale
+                </p>
+                // </div>
             ) : null}
-            {/* {console.log(item.onSale)} */}
-            {/* {console.log(item.deliveryInfo)} */}
-
             <div className="my-4 mx-auto flex w-10/12 flex-col items-start justify-between">
-                <div className="mb-2 flex items-center">
-                    <p className="mr-3 text-sm font-semibold">
-                        {`$${item.price}`}
-                    </p>
-                    {
-                        item.onSale === 'yes' && (<p className="text-red-900 font-bold">on sale</p>)
-                    }
+                <div className="mb-2 flex">
+                    <p
+                        className={`mr-3 text-sm font-semibold ${item.onSale === "yes" ? "line-through" : ""
+                            }`}
+                    >{`$ ${item.price}`}</p>
+                    {item.onSale === "yes" ? (
+                        <p className="mr-3 text-sm font-semibold text-red-700">{`$ ${(
+                            item.price -
+                            item.price * (item.priceDrop / 100)
+                        ).toFixed(2)}`}</p>
+                    ) : null}
+                    {item.onSale === "yes" ? (
+                        <p className="mr-3 text-sm font-semibold">{`-(${item.priceDrop}%)off`}</p>
+                    ) : null}
                 </div>
-                <h3 className="md-3 text-gray-400 text-sm">{item.name}</h3>
+                <h3 className="md-2 text-gray-400 text-sm">{item.name}</h3>
             </div>
         </div>
     )
