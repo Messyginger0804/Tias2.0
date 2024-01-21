ebe2d9165770a7c2542a5b616275de9c1150e075
JC <j.c.ashley4363@gmail.com>
2024-01-21 12:59:14 -0600

    doc(app/middleware/AuthUser.js): created AutherUser.js middleware

---
+++ src/middleware/AuthUser.js
@@ -0,0 +1,14 @@
+
+
+
+export const dynamic = 'force-dynamic';
+
+
+const AuthUser = async (req) => {
+
+    const token = req.headers.get('Authorization');
+
+    console.log(token);
+};
+
+export default AuthUser;
