150ac7ed7f0a8eaf2ed05ae25327c799edd8a084
JC <j.c.ashley4363@gmail.com>
2024-01-29 07:52:06 -0600

    perf(src/app/services/cart/index.js): created getAllCartItems, addToCart and deleteFromCart functions

---
+++ src/services/cart/index.js
@@ -0,0 +1,62 @@
+import Cookies from "js-cookie";
+
+
+export const addToCart = async (formData) => {
+    try {
+        res = await fetch('/api/cart/addToCart', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`,
+            },
+            body: JSON.stringify(formData)
+        });
+
+        const data = await res.json();
+
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const getAllCartItems = async (id) => {
+    try {
+        const res = await fetch(`/api/cart/allCartItems?=${id}`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`,
+            },
+            body: JSON.stringify(formadata)
+        });
+
+        const data = await res.json();
+
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const deleteFromCart = async (id) => {
+    try {
+        const res = await fetch(`/api/cart/deleteFromCart?=${id}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json',
+                Authorization: `Bearer ${Cookies.get('token')}`,
+            },
+            body: JSON.stringify(formadata)
+        });
+
+        const data = await res.json();
+
+        return data;
+
+    } catch (error) {
+        console.error(error);
+    }
+};
