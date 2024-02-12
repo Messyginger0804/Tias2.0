6cd66304950748cc7171bf2c6d001fb07a0ec626
JC <j.c.ashley4363@gmail.com>
2024-02-11 22:51:33 -0600

    perf(src/app/api/order/createOrder/route.js): created the route

---
+++ src/app/api/order/createOrder/route.js
@@ -0,0 +1,46 @@
+import AuthUser from "@/middleware/AuthUser";
+import Cart from "@/models/cart";
+import Order from "@/models/order";
+import connectToDB from "@/mongodb";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+    try {
+        await connectToDB();
+        const isAuthUser = await AuthUser(req);
+
+        if (isAuthUser) {
+            const data = await req.json();
+            const { user } = data;
+
+            const newOrder = await Order.create(data);
+
+            if (newOrder) {
+                await Cart.deleteMany({ userID: user });
+
+                return NextResponse.json({
+                    success: true,
+                    message: "Products are on the way !",
+                });
+            } else {
+                return NextResponse.json({
+                    success: false,
+                    message: "Failed to create a order ! Please try again",
+                });
+            }
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "You are not authticated",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            success: false,
+            message: "Something went wrong in (src/app/api/order/createOrder/route.js) ! Please try again later",
+        });
+    }
+}
